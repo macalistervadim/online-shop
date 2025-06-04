08c321f02d487acfad0f20449fc9b5d1f35366e2
macalistervadim <macalistervadim@yandex.ru>
2025-06-04 20:38:29 +0600

    feat(api): migrate model-requirements to service

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,4 +1,5 @@
 import uuid
+
 import pytest
 import requests
 
@@ -21,19 +22,30 @@ def random_orderid(name=""):
     return f"order-{name}-{random_suffix()}"
 
 
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f"{url}/add_batch",
+        json={
+            "ref": ref,
+            "sku": sku,
+            "qty": qty,
+            "eta": eta,
+        },
+    )
+
+    assert r.status_code == 201
+
+
 @pytest.mark.usefixtures("restart_api")
 def test_happy_path_returns_201_and_allocated_batch(add_stock):
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
-    add_stock(
-        [
-            (laterbatch, sku, 100, "2011-01-02"),
-            (earlybatch, sku, 100, "2011-01-01"),
-            (otherbatch, othersku, 100, None),
-        ]
-    )
+    post_to_add_batch(laterbatch, sku, 100, "2011-01-02")
+    post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
+    post_to_add_batch(otherbatch, othersku, 100, None)
     data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
     url = config.get_api_url()
 
@@ -62,13 +74,15 @@ def test_deallocate():
     url = config.get_api_url()
     # fully allocate
     r = requests.post(
-        f"{url}/allocate", json={"orderid": order1, "sku": sku, "qty": 100}
+        f"{url}/allocate",
+        json={"orderid": order1, "sku": sku, "qty": 100},
     )
     assert r.json()["batchid"] == batch
 
     # cannot allocate second order
     r = requests.post(
-        f"{url}/allocate", json={"orderid": order2, "sku": sku, "qty": 100}
+        f"{url}/allocate",
+        json={"orderid": order2, "sku": sku, "qty": 100},
     )
     assert r.status_code == 400
 
@@ -84,7 +98,8 @@ def test_deallocate():
 
     # now we can allocate second order
     r = requests.post(
-        f"{url}/allocate", json={"orderid": order2, "sku": sku, "qty": 100}
+        f"{url}/allocate",
+        json={"orderid": order2, "sku": sku, "qty": 100},
     )
     assert r.ok
     assert r.json()["batchid"] == batch
